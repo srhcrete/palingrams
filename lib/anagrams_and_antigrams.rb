0bad6ecc315f954f08bc3d300750f02386bbf5ea
Sarah Lindmar <srhcrete@gmail.com>
2017-09-09 23:39:23 -0700

    refactor and pass test to check non-anagram words for letter matches

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -9,16 +9,17 @@ class Palingram
     str2_arr = str2.downcase.split('').sort.join.gsub(/\W/, '')
     str1_reverse = str1.downcase.reverse
     str2_downcase = str2.downcase
+    letter = str1.count(str2)
     if (/[aeiouAEIOU]/.match(str1) === nil || /[aeiouAEIOU]/.match(str2) === nil)
       'These are not words'
     elsif (str1_arr === str2_arr && str1_reverse === str2_downcase)
       'These words are palindromes and anagrams'
     elsif (str1_arr === str2_arr)
       'These words are anagrams'
-    elsif (str1.match(str2) === nil)
-      'These words are antigrams'
+    elsif (letter >=1)
+      'These words are not anagrams, but have ' + letter.to_s + ' matching sets of letters'
     else
-    'These words are not anagrams'
+      'These words are antigrams'
     end
   end
 end
@@ -28,20 +29,4 @@ str1 = gets.chomp
 puts "Type another word"
 str2 = gets.chomp
 example = Palingram.new()
-result = example.is_palingram(str1, str2)
-not_words = 'These are not words'
-palingrams = 'These words are palindromes and anagrams'
-anagrams = 'These words are anagrams'
-antigrams = 'These words are antigrams'
-not_anagrams = 'These words are anagrams'
-if result === not_words
-  puts not_words
-elsif result === palingrams
-  puts palingrams
-elsif result === anagrams
-  puts anagrams
-elsif result === antigrams
-  puts antigrams
-else result === not_anagrams
-  puts not_anagrams
-end
+puts example.is_palingram(str1, str2)
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -20,11 +20,8 @@ describe('#anagrams_and_antigrams') do
   it('will check if a phrase or word is an anagram and also palindrome') do
     expect(example.is_palingram('Lived', 'Devil')).to(eq('These words are palindromes and anagrams'))
   end
-  it('will check if a phrase or word is an antigram') do
-    expect(example.is_palingram('Hi', 'Bye')).to(eq('These words are antigrams'))
-  end
-  it('will ignore punctuation when checking for antigram') do
-    expect(example.is_palingram('Who is this?', 'Bye Bye!')).to(eq('These words are antigrams'))
+  it('will check how many letters non-anagram words have in common') do
+    expect(example.is_palingram('tacocat', 'forgotten')).to(eq('These words are not anagrams, but have 3 matching sets of letters'))
   end
   it('will ignore punctuation when checking for anagram') do
     expect(example.is_palingram('Election results', 'Lies! Let\'s recount')).to(eq('These words are anagrams'))
